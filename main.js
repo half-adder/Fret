a101f4a0e25fce4456a13fb4f133d18ec97f3ec8
Sean Johnsen <seanjohnsen96@gmail.com>
2018-05-26 11:32:51 -0400

    add min and max values to bpm

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var noteMap = {
-    "E": "F2,FS2,G2,GS2,A2,AS2,B2,C2,CS2,D2,DS2,E3".split(','),
-    "A": "AS2,B2,C2,CS2,D2,DS2,E3,F3,FS3,G3,GS3,A3".split(','),
-    "D": "DS2,E3,F3,FS3,G3,GS3,A3,AS3,B3,C3,CS3,D4".split(','),
-    "G": "GS3,A3,AS3,B3,C3,CS3,D4,DS4,E4,F4,FS4,G4".split(','),
-    "B": "C4,CS4,D4,DS4,E5,F5,FS5,G5,GS5,A5,AS5,B5".split(','),
-    "EHi": "F5,FS5,G5,GS5,A5,AS5,B5,C5,CS5,D5,DS5,E6".split(',')
+    "E": "F2,FS2,G2,GS2,A2,AS2,B2,C3,CS3,D3,DS3,E3".split(','),
+    "A": "AS2,B2,C3,CS3,D3,DS3,E3,F3,FS3,G3,GS3,A3".split(','),
+    "D": "DS3,E3,F3,FS3,G3,GS3,A3,AS3,B3,C4,CS4,D4".split(','),
+    "G": "GS3,A3,AS3,B3,C4,CS4,D4,DS4,E4,F4,FS4,G4".split(','),
+    "B": "C4,CS4,D4,DS4,E4,F4,FS4,G4,GS4,A4,AS4,B4".split(','),
+    "EHi": "F4,FS4,G4,GS4,A4,AS4,B4,C5,CS5,D5,DS5,E5".split(',')
 };
 
 var SELECTED_STRING = "E";
@@ -58,6 +58,14 @@ function selectString(S) {
 }
 
 function changeBPM(newValue) {
+    minBPM = 1;
+    maxBPM = 500;
+
+    newValue = Math.max(newValue, minBPM);
+    newValue = Math.min(newValue, maxBPM);
+
+    $("#bpm-input").val(newValue);
+
     Tone.Transport.bpm.rampTo(newValue, 2);
 }
 
